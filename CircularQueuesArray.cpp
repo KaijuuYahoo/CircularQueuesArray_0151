d83334ef8630c5822fece1b727a8e415687f3d04
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-12 14:48:11 +0700

    1. cek apakah antrian penuh

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -5,11 +5,29 @@ using namespace std;
 class Queues
 {
 private:
-    /* data */
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
 public:
-    Queues(/* args */);
-    ~Queues();
-}
+        Queues(){
+        FRONT = -1;
+        REAR = -1;
+    };
+        void insert(){
+        int num;
+        cout << "Enter a number : ";
+        cin >> num;
+        cout << endl;
+
+        //1. cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max-1)||(FRONT == REAR+1))
+        {
+        cout << "Queue Overflow \n";//1a
+
+        }
+        
+    }
+};
 
 int main(int argc, char const *argv[])
 {
