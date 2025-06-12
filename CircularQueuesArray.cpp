b33cf0a43161e367c31819aa960222e2260d2407
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-12 14:44:10 +0700

    Add class Queues

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class Queues
+{
+private:
+    /* data */
+public:
+    Queues(/* args */);
+    ~Queues();
+}
+
 int main(int argc, char const *argv[])
 {
     
